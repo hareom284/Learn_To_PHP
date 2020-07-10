026f1a606697744dbaf704a6ecefdf86afa5c9c5
hareom284 <zawzawwinucsl@gmail.com>
2020-07-10 15:48:03 +0630

    make commit

--- MVC_model/system/classes/route.php
+++ MVC_model/system/classes/route.php
@@ -4,17 +4,17 @@ class route {
     public function __construct()
     {
        $url = $this->url();
-       
+
        print_r($url);
     }
     public function url()
     {
         if(isset($_GET['url']))
         {
-            $url = $_GET['url'];
-            $url = rtrim($url);
-            $url = filter_var($url,FILTER_SANITIZE_URL);
-            $url = explode('/',$url);
+            $url = $_GET['url'];//get the url
+            $url = rtrim($url);//remove the space
+            $url = filter_var($url,FILTER_SANITIZE_URL);//filter the specail character
+            $url = explode('/',$url);//convert it into array
             return $url;
         }
     }
